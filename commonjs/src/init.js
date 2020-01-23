ea81e33bddb35602b75788140448844b3b781b0c
Evgeny Orekhov <eorekhov@axcient.com>
2020-01-23 15:37:45 +0300

    Add support for Node.js 10.17.0
    This is the version that is used on CodeSandbox.

--- commonjs/src/init.js
+++ commonjs/src/init.js
@@ -1,16 +1,14 @@
 "use strict";
 
+require("core-js/modules/es.object.from-entries");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = init;
 const ACTION_ARITY = 2;
 
-function init({
-  state,
-  actions,
-  subscribers
-}) {
+function init({ state, actions, subscribers }) {
   // eslint-disable-next-line fp/no-let
   let currentState = state; // eslint-disable-next-line fp/no-let, init-declarations
 
@@ -18,10 +16,7 @@ function init({
 
   let boundActions;
 
-  function notifySubscribers({
-    actionName,
-    value
-  } = {}) {
+  function notifySubscribers({ actionName, value } = {}) {
     // eslint-disable-next-line fp/no-mutation
     shouldNotifySubscribers = true;
     subscribers.every(subscriber => {
@@ -37,26 +32,30 @@ function init({
     shouldNotifySubscribers = false;
   } // eslint-disable-next-line fp/no-mutation
 
-
-  boundActions = Object.fromEntries(Object.entries(actions).map(([actionName, action]) => [actionName, function boundAction(value) {
-    function getNewState() {
-      if (action.length === ACTION_ARITY) {
-        return action(value, currentState);
-      }
-
-      const partiallyAppliedActionOrNewState = action(currentState);
-      return typeof partiallyAppliedActionOrNewState === "function" ? // Turns out we have a curried action here.
-      // Reapplying arguments in the correct order:
-      action(value)(currentState) : partiallyAppliedActionOrNewState;
-    } // eslint-disable-next-line fp/no-mutation
-
-
-    currentState = getNewState();
-    notifySubscribers({
+  boundActions = Object.fromEntries(
+    Object.entries(actions).map(([actionName, action]) => [
       actionName,
-      value
-    });
-  }]));
+      function boundAction(value) {
+        function getNewState() {
+          if (action.length === ACTION_ARITY) {
+            return action(value, currentState);
+          }
+
+          const partiallyAppliedActionOrNewState = action(currentState);
+          return typeof partiallyAppliedActionOrNewState === "function" // Turns out we have a curried action here.
+            ? // Reapplying arguments in the correct order:
+              action(value)(currentState)
+            : partiallyAppliedActionOrNewState;
+        } // eslint-disable-next-line fp/no-mutation
+
+        currentState = getNewState();
+        notifySubscribers({
+          actionName,
+          value
+        });
+      }
+    ])
+  );
   notifySubscribers();
   return boundActions;
 }
