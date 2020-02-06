88daa5dd6c2a6f2533f0c1aa8c0c01493db5834d
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2020-02-06 23:41:20 +0300

    Fix #6 Add support for slice actions

--- packages/actus/src/init.js
+++ packages/actus/src/init.js
@@ -1,4 +1,14 @@
+import assocPath from "ramda/src/assocPath.js";
+
 const ACTION_ARITY = 2;
+
+function getSlice(object, path) {
+  return path.reduce((acc, property) => acc[property], object);
+}
+
+function setSlice(object, path, slice) {
+  return assocPath(path, slice, object);
+}
 
 export default function init({ state, actions, subscribers }) {
   // eslint-disable-next-line fp/no-let
@@ -27,32 +37,48 @@ export default function init({ state, actions, subscribers }) {
     shouldNotifySubscribers = false;
   }
 
+  // eslint-disable-next-line sonarjs/cognitive-complexity
+  function bindActions(unboundActions, path = []) {
+    return Object.fromEntries(
+      Object.entries(unboundActions).map(([actionName, action]) =>
+        typeof action === "function"
+          ? [
+              actionName,
+              function boundAction(value) {
+                function getNewState() {
+                  const currentSlice = getSlice(currentState, path);
+
+                  if (action.length === ACTION_ARITY) {
+                    const newSlice = action(value, currentSlice);
+
+                    return setSlice(currentState, path, newSlice);
+                  }
+
+                  const partiallyAppliedActionOrNewSlice = action(currentSlice);
+
+                  const newSlice =
+                    typeof partiallyAppliedActionOrNewSlice === "function"
+                      ? // Turns out we have a curried action here.
+                        // Reapplying arguments in the correct order:
+                        action(value)(currentSlice)
+                      : partiallyAppliedActionOrNewSlice;
+
+                  return setSlice(currentState, path, newSlice);
+                }
+
+                // eslint-disable-next-line fp/no-mutation
+                currentState = getNewState();
+
+                notifySubscribers({ actionName, value });
+              }
+            ]
+          : [actionName, bindActions(action, [...path, actionName])]
+      )
+    );
+  }
+
   // eslint-disable-next-line fp/no-mutation
-  boundActions = Object.fromEntries(
-    Object.entries(actions).map(([actionName, action]) => [
-      actionName,
-      function boundAction(value) {
-        function getNewState() {
-          if (action.length === ACTION_ARITY) {
-            return action(value, currentState);
-          }
-
-          const partiallyAppliedActionOrNewState = action(currentState);
-
-          return typeof partiallyAppliedActionOrNewState === "function"
-            ? // Turns out we have a curried action here.
-              // Reapplying arguments in the correct order:
-              action(value)(currentState)
-            : partiallyAppliedActionOrNewState;
-        }
-
-        // eslint-disable-next-line fp/no-mutation
-        currentState = getNewState();
-
-        notifySubscribers({ actionName, value });
-      }
-    ])
-  );
+  boundActions = bindActions(actions);
 
   notifySubscribers();
 
--- packages/actus/src/init.test.js
+++ packages/actus/src/init.test.js
@@ -200,3 +200,79 @@ it("cancels notifying subscribers if an action was called by one of them", () =>
   expect(subscriber2.mock.calls.length).toBe(1);
   expect(subscriber2.mock.calls[0][0].state).toBe(1);
 });
+
+it("supports slice actions", () => {
+  const subscriber = jest.fn();
+
+  const { slice1, slice2 } = init({
+    state: { slice1: 0, slice2: 0 },
+    actions: {
+      slice1: {
+        inc: state => state + 1
+      },
+      slice2: {
+        add: (value, state) => state + value,
+        subtract: value => state => state - value
+      }
+    },
+    subscribers: [subscriber]
+  });
+
+  slice1.inc();
+  slice2.add(4);
+  slice2.subtract(8);
+
+  expect(subscriber.mock.calls.length).toBe(4);
+  expect(subscriber.mock.calls[1][0].state).toEqual({ slice1: 1, slice2: 0 });
+  expect(subscriber.mock.calls[2][0].state).toEqual({ slice1: 1, slice2: 4 });
+  expect(subscriber.mock.calls[3][0].state).toEqual({ slice1: 1, slice2: -4 });
+});
+
+it("supports recursive slice actions", () => {
+  const subscriber = jest.fn();
+
+  const { slice } = init({
+    state: {
+      slice: {
+        subslice1: 0,
+        subslice2: 0
+      }
+    },
+    actions: {
+      slice: {
+        subslice1: {
+          inc: state => state + 1
+        },
+        subslice2: {
+          add: (value, state) => state + value,
+          subtract: value => state => state - value
+        }
+      }
+    },
+    subscribers: [subscriber]
+  });
+
+  slice.subslice1.inc();
+  slice.subslice2.add(4);
+  slice.subslice2.subtract(8);
+
+  expect(subscriber.mock.calls.length).toBe(4);
+  expect(subscriber.mock.calls[1][0].state).toEqual({
+    slice: {
+      subslice1: 1,
+      subslice2: 0
+    }
+  });
+  expect(subscriber.mock.calls[2][0].state).toEqual({
+    slice: {
+      subslice1: 1,
+      subslice2: 4
+    }
+  });
+  expect(subscriber.mock.calls[3][0].state).toEqual({
+    slice: {
+      subslice1: 1,
+      subslice2: -4
+    }
+  });
+});
