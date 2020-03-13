f36812524426e5d9b6c4713d5d82f0902c95e6b2
Evgeny Orekhov <eorekhov@axcient.com>
2020-03-13 19:38:31 +0300

    Fix #5 Add Redux DevTools Extension plugin

---
+++ packages/actus-redux-devtools/README.md
@@ -0,0 +1,61 @@
+# actus-redux-devtools
+
+Use [Redux DevTools Extension](https://github.com/zalmoxisus/redux-devtools-extension) with actus
+
+[![npm version](https://img.shields.io/npm/v/actus-redux-devtools.svg?style=flat-square)](https://www.npmjs.com/package/actus-redux-devtools)
+
+## Install
+
+```sh
+npm install actus-redux-devtools
+```
+
+## Examples
+
+### Usage
+
+```js
+import { init } from "actus";
+import reduxDevTools from "actus-redux-devtools";
+
+init([
+  reduxDevTools({ name: "Optional instance name" }),
+  {
+    state: {...},
+    actions: {...},
+    subscribers: [...]
+  }
+]);
+```
+
+### React Counter App
+
+```js
+import { init } from "actus";
+import reduxDevTools from "actus-redux-devtools";
+import React from "react";
+import ReactDOM from "react-dom";
+
+init([
+  reduxDevTools(),
+  {
+    state: 0,
+    actions: {
+      inc: (value, state) => state + 1,
+      dec: (value, state) => state - 1
+    },
+    subscribers: [
+      ({ state, actions }) => {
+        ReactDOM.render(
+          <>
+            <h1>{state}</h1>
+            <button onClick={actions.inc}>+</button>
+            <button onClick={actions.dec}>-</button>
+          </>,
+          document.querySelector("#root")
+        );
+      }
+    ]
+  }
+]);
+```
---
+++ packages/actus-redux-devtools/index.js
@@ -0,0 +1,145 @@
+/*global window */
+
+function getSlice(object, path) {
+  return path.reduce(
+    (acc, property) =>
+      acc === undefined || acc === null ? undefined : acc[property],
+    object
+  );
+}
+
+// eslint-disable-next-line sonarjs/cognitive-complexity
+export default function reduxDevTools({ name = undefined } = {}) {
+  if (
+    typeof window === "undefined" ||
+    window.__REDUX_DEVTOOLS_EXTENSION__ === undefined
+  ) {
+    return {};
+  }
+
+  return {
+    actions: {
+      setStateFromDevTools: value => value
+    },
+
+    subscribers: [
+      (function makeReduxDevtoolsSubscriber() {
+        /* eslint-disable fp/no-let, init-declarations */
+        let initialState;
+        let currentState;
+        let devTools;
+        /* eslint-enable fp/no-let, init-declarations */
+
+        function parse(value) {
+          try {
+            return JSON.parse(value);
+          } catch (error) {
+            devTools.error(`Invalid JSON: ${value}`);
+
+            throw error;
+          }
+        }
+
+        return function send({ state, actionName, value, actions }) {
+          // eslint-disable-next-line fp/no-mutation
+          currentState = state;
+
+          if (devTools === undefined) {
+            // eslint-disable-next-line fp/no-mutation
+            initialState = state;
+
+            // eslint-disable-next-line fp/no-mutation
+            devTools = window.__REDUX_DEVTOOLS_EXTENSION__.connect({
+              name,
+              actionCreators: actions,
+              actionsBlacklist: ["setStateFromDevTools"],
+              features: {
+                pause: true, // start/pause recording of dispatched actions
+                lock: false, // lock/unlock dispatching actions and side effects
+                persist: false, // persist states on page reloading
+                export: false, // export history of actions in a file
+                import: false, // import history of actions from a file
+                jump: true, // jump back and forth (time travelling)
+                skip: false, // skip (cancel) actions
+                reorder: false, // drag and drop actions in the history list
+                dispatch: true, // dispatch custom actions or action creators
+                test: false // generate tests for the selected actions
+              }
+            });
+
+            // eslint-disable-next-line max-statements, complexity
+            devTools.subscribe(message => {
+              if (message.type === "ACTION") {
+                const payload =
+                  typeof message.payload === "string"
+                    ? parse(message.payload)
+                    : message.payload;
+
+                if (payload.name === undefined) {
+                  devTools.error(
+                    `Invalid action: ${message.payload}.
+                    Example: { "name": "foo.bar", "args": ["{ \\"baz\\": \\"qux\\" }"] }`
+                  );
+
+                  return;
+                }
+
+                const action = getSlice(actions, payload.name.split("."));
+
+                if (typeof action === "function") {
+                  action(
+                    Array.isArray(payload.args) && payload.args.length === 0
+                      ? undefined
+                      : parse(payload.args[0])
+                  );
+
+                  return;
+                }
+
+                devTools.error(`Unknown action: ${payload.name}`);
+
+                return;
+              }
+
+              if (message.type === "DISPATCH") {
+                // eslint-disable-next-line default-case
+                switch (message.payload.type) {
+                  case "RESET":
+                    actions.setStateFromDevTools(initialState);
+                    devTools.init(initialState);
+
+                    return;
+                  case "COMMIT":
+                    devTools.init(currentState);
+
+                    return;
+                  case "ROLLBACK": {
+                    const commitedState = parse(message.state);
+
+                    actions.setStateFromDevTools(commitedState);
+                    devTools.init(commitedState);
+
+                    return;
+                  }
+                  case "JUMP_TO_STATE":
+                  case "JUMP_TO_ACTION":
+                    actions.setStateFromDevTools(parse(message.state));
+                }
+              }
+            });
+
+            devTools.init(state);
+
+            return;
+          }
+
+          const type = Array.isArray(actionName)
+            ? actionName.join(".")
+            : actionName;
+
+          devTools.send({ type, payload: value }, state);
+        };
+      })()
+    ]
+  };
+}
