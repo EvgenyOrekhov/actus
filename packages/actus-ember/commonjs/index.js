070d44e362134a912e7e4ba1042cd59c459d06af
Evgeny Orekhov <eorekhov@axcient.com>
2020-05-16 00:59:26 +0300

    Publish
     - actus-ember@0.2.0-alpha.0

--- packages/actus-ember/commonjs/index.js
+++ packages/actus-ember/commonjs/index.js
@@ -13,23 +13,27 @@ var _actusDefaultActions = _interopRequireDefault(require("actus-default-actions
 
 var _deepFreeze = _interopRequireDefault(require("deep-freeze"));
 
+var _isPlainObj = _interopRequireDefault(require("is-plain-obj"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function actusify(target, {
-  isDevelopment = true,
-  plugins = []
+function actusify(config, {
+  isDevelopment = true
 } = {}) {
-  // eslint-disable-next-line fp/no-mutation, no-param-reassign
-  target.actions = (0, _actus.init)([isDevelopment && (0, _actusLogger.default)({
-    name: target.constructor.name
-  }), (0, _actusDefaultActions.default)(target.state), ...plugins, {
+  const plugins = Array.isArray(config) ? config : [config];
+  const target = plugins.find(plugin => !(0, _isPlainObj.default)(plugin));
+  const normalizedPlugins = plugins.map(plugin => plugin === target ? {
     state: target.state,
     actions: target.actions || {},
     subscribers: [({
       state
     }) => {
-      // eslint-disable-next-line fp/no-mutation, no-param-reassign
+      // eslint-disable-next-line fp/no-mutation
       target.state = isDevelopment ? (0, _deepFreeze.default)(state) : state;
     }]
-  }]);
+  } : plugin); // eslint-disable-next-line fp/no-mutation
+
+  target.actions = (0, _actus.init)([isDevelopment && (0, _actusLogger.default)({
+    name: target.constructor.name
+  }), (0, _actusDefaultActions.default)(target.state), ...normalizedPlugins]);
 }
