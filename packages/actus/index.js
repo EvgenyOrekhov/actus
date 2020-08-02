e97f1e34d52b66e86e2a880bcb553cd7068d9181
Evgeny Orekhov <eorekhov@axcient.com>
2020-08-03 00:27:29 +0300

    Move actus-logger to the main package

--- packages/actus/index.js
+++ packages/actus/index.js
@@ -1,5 +1,6 @@
 import actus from "./src/init.js";
 import defaultActions from "./src/plugins/defaultActions/index.js";
 import persist from "./src/plugins/persist/index.js";
+import logger from "./src/plugins/logger/index.js";
 
-export { actus, defaultActions, persist };
+export { actus, defaultActions, persist, logger };
