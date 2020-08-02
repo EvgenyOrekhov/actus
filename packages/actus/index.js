c2872f045dcf58f7a0401650ad2d85b6f8425322
Evgeny Orekhov <eorekhov@axcient.com>
2020-08-03 02:07:43 +0300

    Export freeze

--- packages/actus/index.js
+++ packages/actus/index.js
@@ -2,5 +2,6 @@ import actus from "./src/actus.js";
 import defaultActions from "./src/plugins/defaultActions/index.js";
 import persist from "./src/plugins/persist/index.js";
 import logger from "./src/plugins/logger/index.js";
+import freeze from "./src/plugins/freeze/index.js";
 
-export { actus, defaultActions, persist, logger };
+export { actus, defaultActions, persist, logger, freeze };
