7052ef6037683f74cf3954604bbe097f6753dee8
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2021-12-01 01:05:21 +0300

    Update dependencies, lint

--- packages/actus/index.js
+++ packages/actus/index.js
@@ -1,8 +1,6 @@
-import actus from "./src/actus.js";
-import defaultActions from "./src/plugins/defaultActions/index.js";
-import persist from "./src/plugins/persist/index.js";
-import logger from "./src/plugins/logger/index.js";
-import freeze from "./src/plugins/freeze/index.js";
-import reduxDevTools from "./src/plugins/reduxDevTools/index.js";
-
-export { actus, defaultActions, persist, logger, freeze, reduxDevTools };
+export { default as actus } from "./src/actus.js";
+export { default as defaultActions } from "./src/plugins/defaultActions/index.js";
+export { default as persist } from "./src/plugins/persist/index.js";
+export { default as logger } from "./src/plugins/logger/index.js";
+export { default as freeze } from "./src/plugins/freeze/index.js";
+export { default as reduxDevTools } from "./src/plugins/reduxDevTools/index.js";
